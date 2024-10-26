79f9d2c7e533e86a1a60033555b3f4ebcb3f1edf
carrot-c4k3 <emma.kirkpatrick.1994@gmail.com>
2024-10-26 13:29:35 -0700

    reluctantly switch to unicode

--- stfstool/stfstool.cpp
+++ stfstool/stfstool.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <locale>
+#include <codecvt>
 
 #include <stdio.h>
 #include <Stfs/StfsPackage.h>
@@ -20,9 +22,26 @@ typedef enum _StfsOpenMode {
 } StfsOpenMode;
 
 typedef struct _FileInjectInfo {
-	std::string path;
+	std::wstring path;
 	std::string nameInPackage;
 } FileInjectInfo;
+
+// From https://stackoverflow.com/questions/4804298/how-to-convert-wstring-into-string
+std::wstring s2ws(const std::string& str)
+{
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+	return converterX.from_bytes(str);
+}
+
+std::string ws2s(const std::wstring& wstr)
+{
+	using convert_typeX = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_typeX, wchar_t> converterX;
+
+	return converterX.to_bytes(wstr);
+}
 
 bool ReadEntireFile(std::string path, BYTE** contents, size_t* size)
 {
@@ -46,48 +65,71 @@ bool ReadEntireFile(std::string path, BYTE** contents, size_t* size)
 }
 
 
-int main(int argc, char** argv)
+int wmain(int argc, wchar_t** argv)
 {
 	StfsOpenMode openMode = Unknown;
 	std::string packagePath;
 	std::string kvPath;
+	BYTE profileId[8] = { 0 };
 	std::vector<FileInjectInfo> filesToInject;
 
 	for (int i = 0; i < argc; i++)
 	{
-		std::string cur_arg(argv[i]);
+		std::wstring cur_arg(argv[i]);
 
 		// open mode
-		if (cur_arg == "-c" || cur_arg == "--create")
+		if (cur_arg == L"-c" || cur_arg == L"--create")
 		{
 			openMode = Create;
 
 			NEXT_ARG("--create");
-			packagePath = std::string(argv[i]);
+			std::wstring wpackagePath(argv[i]);
+			packagePath = ws2s(wpackagePath);
 		}
-		else if (cur_arg == "-o" || cur_arg == "--open")
+		else if (cur_arg == L"-o" || cur_arg == L"--open")
 		{
 			openMode = OpenExisting;
 
 			NEXT_ARG("--open");
-			packagePath = std::string(argv[i]);
+			std::wstring wpackagePath(argv[i]);
+			packagePath = ws2s(wpackagePath);
 		}
-		else if (cur_arg == "-kv" || cur_arg == "--keyvault")
+		else if (cur_arg == L"-kv" || cur_arg == L"--keyvault")
 		{
 			NEXT_ARG("--keyvault");
-			kvPath = std::string(argv[i]);
+			std::wstring wkvPath(argv[i]);
+			kvPath = ws2s(wkvPath);
 		}
-		else if (cur_arg == "-if" || cur_arg == "--inject-file")
+		else if (cur_arg == L"-if" || cur_arg == L"--inject-file")
 		{
 			FileInjectInfo injectInfo;
 
 			NEXT_ARG("--inject_file");
-			injectInfo.path = std::string(argv[i]);
+			injectInfo.path = std::wstring(argv[i]);
 
 			NEXT_ARG("--inject_file");
-			injectInfo.nameInPackage = std::string(argv[i]);
+			std::wstring nameInPackage(argv[i]);
+			injectInfo.nameInPackage = ws2s(nameInPackage);
 
 			filesToInject.push_back(injectInfo);
+		}
+		else if (cur_arg == L"-pid" || cur_arg == L"--profile-id")
+		{
+			NEXT_ARG("--profile-id");
+			std::wstring profileIdStr(argv[i]);
+
+			if (profileIdStr.length() != 0x10)
+			{
+				printf("Error: Profile ID should be 16 characters long, "
+					"provided %i (--profile-id)\n", profileIdStr.length());
+
+				return 0;
+			}
+
+			swscanf_s(profileIdStr.c_str(),
+				L"%02X%02X%02X%02X%02X%02X%02X%02X",
+				&profileId[0], &profileId[1], &profileId[2], &profileId[3],
+				&profileId[4], &profileId[5], &profileId[6], &profileId[7]);
 		}
 	}
 
@@ -130,7 +172,6 @@ int main(int argc, char** argv)
 	package.metaData->discNumber = 1;
 	package.metaData->discInSet = 1;
 
-	BYTE profileId[] = { 0xE0, 0x00, 0x04, 0xFD, 0xAE, 0xE4, 0x01, 0x7C };
 	memcpy(package.metaData->profileID, profileId, sizeof(profileId));
 
 	package.metaData->thumbnailImage = thumbnail_data;
@@ -140,7 +181,7 @@ int main(int argc, char** argv)
 	package.metaData->titleThumbnailImageSize = thumbnail_size;
 
 	package.metaData->WriteMetaData();
-	
+
 	package.InjectFile("exploit.bin", "savegame.svg");
 
 	package.Resign(kvPath);
